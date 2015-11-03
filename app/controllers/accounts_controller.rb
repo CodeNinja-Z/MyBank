be90d7d25688d38abebd9539cd082e3c8c0f920f
Jill Cates <cates.jill@gmail.com>
2015-11-02 22:08:54 -0500

    Task and account controller updated

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,2 +1,22 @@
 class AccountsController < ApplicationController
+
+	def create
+		@account = Account.new(account_params)
+	end 
+
+	def index
+	end 
+
+	def show
+	end 
+
+	def add_child
+	end 
+
+	private
+
+	def account_params
+		params.require(:account).permit(:total_amount, :parent_id, :child_id)
+	end 
+
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,28 @@
 class TasksController < ApplicationController
+
+	def create
+		@task = Task.new(task_params)
+	end 
+
+	def index
+	end 
+
+	def update
+	end 
+
+	def show 
+	end 
+
+
+	def destroy
+		@task = Task.find(params[:id])
+		@task.destroy
+	end 
+
+	private 
+	def task_params
+		params.require(:task).permit(:name, :description, :deadline, :amount)
+	end 
+
+
 end
