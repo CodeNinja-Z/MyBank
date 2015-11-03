62eff413934962f3e3c2ac6870683a1e23596ce9
Jill Cates <cates.jill@gmail.com>
2015-11-02 22:15:51 -0500

    Transaction and request controller updated

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,2 +1,16 @@
 class RequestsController < ApplicationController
+
+	def create
+		@request = Request.new(request_params)
+	end 
+
+	def index
+	end 
+
+	private 
+
+	def request_params
+		params.require(:request).permit(:name, :type, :amount, :description, :date, :account_id, :child_id)
+	end 
+
 end
--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,2 +1,16 @@
 class TransactionsController < ApplicationController
+
+	def create
+		@transaction = Transaction.new(transaction_params)
+	end 
+
+	def index
+	end 
+
+	private 
+
+	def request_params
+		params.require(:request).permit(:name, :type, :amount, :description, :date, :account_id, :parent_id)
+	end 
+
 end
